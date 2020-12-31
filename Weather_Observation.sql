d54bf9fa04fa434b2c7da381554e808a5c7a8097
Bharti Parekh <bella.parekh.code@gmail.com>
2020-12-31 16:09:14 -0600

    Weather Observation Station
    Weather Observation Station

---
+++ Weather_Observation.sql
@@ -0,0 +1,6 @@
+Weather Observation Station 3
+Query a list of CITY names from STATION for cities that have an even ID number. Print the results in any order, but exclude duplicates from the answer.
+
+Solution:
+SELECT DISTINCT CITY FROM STATION WHERE MOD(ID,2)=0;
+
